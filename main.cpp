0fbbe30f85cdf93619dd4d390f2c323b16e78813
Jose Eduardo <a01710605@tec.mx>
2023-10-16 08:42:30 -0600

    ...

---
+++ main.cpp
@@ -0,0 +1,45 @@
+/*
+* Autor: jeviverose
+* Main
+*
+*/
+
+#include <iostream>
+#include "arbolgenealogico.h"
+#include "personaje.h"
+
+int main() {
+    
+    arbolgenealogico arbol;
+
+    // Cargar datos desde un archivo 
+    arbol.cargarDesdeArchivo("datos.txt");
+
+    // Implementar la interfaz de usuario
+    int opcion;
+    do {
+        std::cout << "1. Buscar personaje\n";
+        std::cout << "2. Imprimir árbol\n";
+        std::cout << "3. Salir\n";
+        std::cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                std::cout << "Introduce el nombre del personaje a buscar: ";
+                std::string nombre;
+                std::cin >> nombre;
+                arbol.buscarPersonaje(nombre);
+                break;
+            case 2:
+                arbol.imprimirArbol();
+                break;
+            case 3:
+                std::cout << "Saliendo del programa.\n";
+                break;
+            default:
+                std::cout << "Opción no válida. Intente nuevamente.\n";
+        }
+    } while (opcion != 3);
+
+    return 0;
+}
