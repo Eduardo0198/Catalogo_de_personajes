ddd3914a28240358334857a22ee5fcf78a82d982
Jose Eduardo <a01710605@tec.mx>
2023-10-16 08:42:13 -0600

    se cambia la estructura del proyecto por un bst

---
+++ arbolgenealogico.h
@@ -0,0 +1,54 @@
+/*
+* Autor: jeviverose
+* Clase Arbol
+*
+*/
+
+#ifndef ARBOL_H_
+#define ARBOL_H_
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "personaje.h"
+
+class arbolgenealogico {
+public:
+    personaje* raiz;
+
+    arbolgenealogico();
+
+    // Cargar datos desde un archivo 
+    void cargarDesdeArchivo(const std::string& archivo);
+
+    // Buscar un personaje en el árbol
+    void buscarPersonaje(const std::string& nombre);
+
+    // Imprimir el árbol genealógico
+    void imprimirArbol();
+
+    // Agregar un personaje al árbol
+    void agregarPersonaje(personaje* personaje);
+
+    arbolgenealogico::arbolgenealogico() : raiz(nullptr){
+    }
+
+    void arbolgenealogico::cargarDesdeArchivo(const std::string& archivo){
+    }
+
+    void arbolgenealogico::buscarPersonaje(const std::string& nombre) {
+    }
+
+    void arbolgenealogico::imprimirArbol() {
+    }
+    
+    void arbolgenealogico::agregarPersonaje(personaje* personaje) {
+    if (raiz == nullptr) {
+        raiz = personaje;
+    } 
+    else {}
+    }
+};
+
+
+#endif
---
+++ personaje.h
@@ -0,0 +1,35 @@
+/*
+* Autor: jeviverose
+* Clase Personaje
+*
+*/
+
+#ifndef PERSONAJES_H_
+#define PERSONAJES_H_
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+class personaje {
+    
+    public:
+    std::string nombre; 
+    std::string casa; 
+    std::string emblema; 
+    int ano_nacimiento; 
+    std::string linaje; 
+    std::vector<personaje*> hijos;
+
+    
+    Personaje::Personaje(const std::string& nombre, const std::string& casa, const std::string& biografia)
+         :nombre(nombre), casa(casa), biografia(biografia), padre(nullptr) {}
+
+    void Personaje::agregarHijo(Personaje* hijo) {
+         hijos.push_back(hijo);
+         hijo->padre = this;
+};
+
+
+
+#endif
