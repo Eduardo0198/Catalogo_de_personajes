8dedb1042ce4841b97460456baf2ad29900e280d
Eduardo Viveros <111369460+Eduardo0198@users.noreply.github.com>
2023-10-16 09:21:49 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,53 @@
-//
+/*
+* Autor: jeviverose
+* Clase Arbol
+*
+*/
 
+#ifndef ARBOL_H_
+#define ARBOL_H_
 #include <iostream>
 #include <string>
 #include <vector>
-#include <list>
-#include <algorithm> // Para std::shuffle
-#include <random>  
-#include "Personajes.h"
- 
+#include <fstream>
+#include "personaje.h"
 
-int main() {
+class arbolgenealogico {
+public:
+    personaje* raiz;
 
-   //Padre
-    // Objeto declarado como personaje
-   Personaje Reyloco("Aerys", "Targaryen", "Dragon", 215, "legitimo");
-   
+    arbolgenealogico();
 
-    // Imprime el nombre del personaje 1 utilizando el getter
-   cout << "\n\nARBOL GENEALOGICO DE LOS TARGERYAN"<<endl;
-   cout << "\n\nNombre: " << Reyloco.getNombre() << endl;
-   cout << "Casa: " << Reyloco.getCasa() << endl;
-   cout << "Animal: " << Reyloco.getAnimal() << endl;
-   cout << "Ano de nacimiento: " << Reyloco.getAno_nacimiento() << endl;
-   cout << "Linaje: " << Reyloco.getLinaje() << endl;
+    // Cargar datos desde un archivo 
+    void cargarDesdeArchivo(const std::string& archivo);
 
-   //Madre
-  
-   // Hijos
+    // Buscar un personaje en el árbol
+    void buscarPersonaje(const std::string& nombre);
 
-   Personaje Rhaegar("Rhaegar", "Targaryen", "Dragon",259, "legitimo" );
-   Personaje Viserys("Viserys", "Targaryen", "Dragon",276, "legitimo" );
-   Personaje Deanerys("Deanerys", "Targaryen", "Dragon",284, "legitimo" );
+    // Imprimir el árbol genealógico
+    void imprimirArbol();
 
-   //Vector que representa los hijos del rey loco Aerys 
-   vector<Personaje*> hijos = {&Viserys, &Rhaegar,&Deanerys};
-  
+    // Agregar un personaje al árbol
+    void agregarPersonaje(personaje* personaje);
 
-   std::cout << "Informacion de los hijos antes de ordenar:" << std::endl;
-   
-   Reyloco.printInfo(hijos);
-   // se usa el emetodo de ordenamiento 
-   Reyloco.bubbleSort(hijos);
-   
-   std::cout << "Informacion de los hijos despues de ordenar:" << std::endl;
+    arbolgenealogico::arbolgenealogico() : raiz(nullptr){
+    }
+
+    void arbolgenealogico::cargarDesdeArchivo(const std::string& archivo){
+    }
+
+    void arbolgenealogico::buscarPersonaje(const std::string& nombre) {
+    }
+
+    void arbolgenealogico::imprimirArbol() {
+    }
     
-   Reyloco.printInfo(hijos);
-   
-
-   return 0;
-
-}
+    void arbolgenealogico::agregarPersonaje(personaje* personaje) {
+    if (raiz == nullptr) {
+        raiz = personaje;
+    } 
+    else {}
+    
+    }
+};
+#endif
